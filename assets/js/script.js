efed82bca644a7b565b2bda31fe32bad3033ba9e
Mona Lisa <holmesmainsend@gmail.com>
2022-11-07 00:23:05 -0500

    started grabbing DOM elements

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const usernameEl = document.getElementById("username-el")
+const passwordEl = document.getElementById("password-el")
+const loginEl = document.getElementById("login-el")
+
+loginEl.addEventListener("click", function() {
+    console.log("The button has been clicked!")
+})
