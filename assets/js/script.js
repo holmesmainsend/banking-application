258e8d570596098f064435c2fda7ea5895646fa4
Mona Lisa <holmesmainsend@gmail.com>
2022-11-07 14:36:20 -0500

    created new test hash process

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,21 @@ const usernameEl = document.getElementById("username-el")
 const passwordEl = document.getElementById("password-el")
 const loginEl = document.getElementById("login-el")
 
+class HashTable {
+    constructor(username, password) {
+        this.username = username
+        this.hash = password + "bananas"
+    }
+}
 
+let user1 = new HashTable("Alice", "Mypassword123!")
+console.log(user1.hash)
 
+let user2 = new HashTable("Steve", "Thisisthepassword@#")
+console.log(user2.hash)
 
+let user3 = new HashTable("Joe", "Metsamillion67")
+console.log(user3.hash)
 
 
 
