1823ed3280853c85b65b00452c81345fdd28080d
Mona Lisa <holmesmainsend@gmail.com>
2022-11-07 02:23:36 -0500

    commented out unnecessary code for now

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,12 @@ const usernameEl = document.getElementById("username-el")
 const passwordEl = document.getElementById("password-el")
 const loginEl = document.getElementById("login-el")
 
+// Hardcoded user credentials... for now
+let originalUsername = "Alice"
+let originalPassword = "Mypassword123!"
+
 loginEl.addEventListener("click", function() {
-    localStorage.setItem(usernameEl.value, passwordEl.value)
+
+//    localStorage.setItem(usernameEl.value, passwordEl.value)
 //    window.location.reload()
 })
