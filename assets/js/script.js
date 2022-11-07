a96abf8c66341954033c9b095a18095cca61d3e9
Mona Lisa <holmesmainsend@gmail.com>
2022-11-07 12:19:26 -0500

    tested rudimentary, incorrect hashing function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,27 +2,33 @@ const usernameEl = document.getElementById("username-el")
 const passwordEl = document.getElementById("password-el")
 const loginEl = document.getElementById("login-el")
 
-class testHash {
+class hashTable {
     constructor() {
         this.table = new Array(127)
         this.size = 0
     }
+    
+    testHash(password) {
+        let hash = 0
+        for(let i = 0; i < password.length; i++) {
+            hash += password.charCodeAt(i)
+        }
+        return hash % this.table.length
+    }
+
+    set(password, value) {
+        const index = this.testHash(password)
+        this.table[index] = [password, value]
+        this.size++
+    }
 }
 
-const users = new Map()
-users.set("Alice", "Mypassword123!")
-
-// Hardcoded user credentials... for now
-let user = {
-    Username: "Alice",
-    Password: "Mypassword123!"
-}
-
-
+const hash1 = new hashTable()
+hash1.set("Alice", "Mypassphrase123")
+console.log(hash1.testHash("Alice"))
 
 
 loginEl.addEventListener("click", function() {
-
 //    localStorage.setItem(usernameEl.value, passwordEl.value)
 //    window.location.reload()
 })
