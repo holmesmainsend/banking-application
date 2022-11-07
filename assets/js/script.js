b19eb53653dc243d3020ed8a7e93010f72a33b15
Mona Lisa <holmesmainsend@gmail.com>
2022-11-07 12:04:06 -0500

    began adding hash function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,24 @@ const usernameEl = document.getElementById("username-el")
 const passwordEl = document.getElementById("password-el")
 const loginEl = document.getElementById("login-el")
 
+class testHash {
+    constructor() {
+        this.table = new Array(127)
+        this.size = 0
+    }
+}
+
+const users = new Map()
+users.set("Alice", "Mypassword123!")
+
 // Hardcoded user credentials... for now
-let originalUsername = "Alice"
-let originalPassword = "Mypassword123!"
+let user = {
+    Username: "Alice",
+    Password: "Mypassword123!"
+}
+
+
+
 
 loginEl.addEventListener("click", function() {
 
